9f96d1f44020d2de7439ec346040e5d36feccead
Barnabás BARTHA <barnabas@barthaweb.com>
2021-02-21 19:47:09 +0100

    Add details to clouds

--- Assets/Scripts/CloudGenerator.cs
+++ Assets/Scripts/CloudGenerator.cs
@@ -11,6 +11,7 @@ public class CloudGenerator : MonoBehaviour {
    public float onDuration;
    public float offDuration;
    public float durationOffsetPerCloud;
+   public float moveX;
 
    private List<GameObject> clouds;
 
@@ -19,28 +20,33 @@ public class CloudGenerator : MonoBehaviour {
 
       for (var n = 0; n < size; n++) {
          var cloud = Instantiate(cloudPrefab, new Vector3(), Quaternion.identity);
-         InitPosition(cloud, Rand(), Rand(), Rand());
+         cloud.transform.parent = transform;
          InitTween(cloud, n);
          clouds.Add(cloud);
       }
    }
 
-   private void InitPosition(GameObject cloud, float x, float y, float z) {
-      var thisTransform = transform;
-      cloud.transform.parent = thisTransform;
-      var position = thisTransform.position;
+   private void InitPosition(GameObject cloud) {
+      var position = transform.position;
       cloud.transform.position = new Vector3(
-         position.x + x * positionScatter / 2,
-         position.y + y * positionScatter / 6,
-         position.z + z * positionScatter / 2
+         position.x + Rand() * positionScatter / 2,
+         position.y + Rand() * positionScatter / 6,
+         position.z + Rand() * positionScatter / 2
       );
    }
 
    private void InitTween(GameObject cloud, float n, float delay = 0) {
       cloud.transform.localScale = Vector3.zero;
+      InitPosition(cloud);
       cloud.transform.DOScale(Vector3.one, scaleDuration)
+         .SetLoops(0)
+         .SetDelay(delay > 0 ? delay : durationOffsetPerCloud * n);
+      cloud.transform.DOMoveX(cloud.transform.position.x + moveX, scaleDuration * 2 + onDuration)
+         .SetLoops(0)
+         .SetEase(Ease.Linear)
          .SetDelay(delay > 0 ? delay : durationOffsetPerCloud * n);
       cloud.transform.DOScale(Vector3.zero, scaleDuration)
+         .SetLoops(0)
          .SetDelay(delay > 0 ? delay + onDuration + scaleDuration : durationOffsetPerCloud * n)
          .OnComplete(() => { InitTween(cloud, n, offDuration); });
    }
